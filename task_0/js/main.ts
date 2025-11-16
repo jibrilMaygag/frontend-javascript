c8ac5fbebc69fc7c80b0ec80b7b86eefb0fd3801
Jibril <jibrilmeygag@gmail.com>
2025-11-17 00:38:33 +0300

    fix bug

---
+++ task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+    firstname:string;
+    lastname:string;
+    age:number;
+    location:string;
+}
+let student1:Student={
+    firstname:"Jibril",
+    lastname:"Abdi",
+    age:21,
+    location:"Addis Ababa"
+
+}
+let student2:Student={
+    firstname:"Kenean",
+    lastname:"Ayalew",
+    age:20,
+    location:"Addis Ababa"
+
+}
+let studentsList:Student[]=[student1,student2]
